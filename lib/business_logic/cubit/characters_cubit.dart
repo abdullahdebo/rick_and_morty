45d107df46bb286dd308e354d072e7809fd1dc51
ABDULLAH DEBO <abd.valancia2@gmail.com>
2024-07-11 10:32:22 +0300

    Implement state and cubit

--- lib/business_logic/cubit/characters_cubit.dart
+++ lib/business_logic/cubit/characters_cubit.dart
@@ -1,8 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first, depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+
+import 'package:movieapp/data/models/characters.dart';
+import 'package:movieapp/data/repository/characters_repository.dart';
 
 part 'characters_state.dart';
 
 class CharactersCubit extends Cubit<CharactersState> {
-  CharactersCubit() : super(CharactersInitial());
+  final CharactersRepository charactersRepository;
+  late List<Character> characters;
+  CharactersCubit(
+    this.charactersRepository,
+  ) : super(CharactersInitial());
+
+  List<Character> getAllCharacters() {
+    charactersRepository.getAllCharacters().then((characters) {
+      emit(CharactersLoaded(characters));
+      this.characters = characters;
+    });
+
+    return characters;
+  }
 }
--- lib/business_logic/cubit/characters_state.dart
+++ lib/business_logic/cubit/characters_state.dart
@@ -4,3 +4,9 @@ part of 'characters_cubit.dart';
 sealed class CharactersState {}
 
 final class CharactersInitial extends CharactersState {}
+
+class CharactersLoaded extends CharactersState {
+  final List<Chracter> characters;
+
+  CharactersLoaded(this.characters);
+}
