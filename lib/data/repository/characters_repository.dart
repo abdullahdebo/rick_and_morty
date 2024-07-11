ec9677b949e006983f65c80afde8e3da3e03a16f
ABDULLAH DEBO <abd.valancia2@gmail.com>
2024-07-11 10:09:54 +0300

    implement repository and web_services

---
+++ lib/data/repository/characters_repository.dart
@@ -0,0 +1,15 @@
+import 'package:movieapp/data/models/characters.dart';
+import 'package:movieapp/data/web_services/characters_web_services.dart';
+
+class CharactersRepository {
+  final CharactersWebServices charactersWebServices;
+
+  CharactersRepository(this.charactersWebServices);
+
+  Future<List<dynamic>> getAllCharacters() async {
+    final characters = await charactersWebServices.getAllCharacters();
+    return characters
+        .map((character) => Character.fromJson(character))
+        .toList();
+  }
+}
